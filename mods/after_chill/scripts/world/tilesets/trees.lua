41ceb8109104db712ba316f8e5bb482a1ee155a3
DragonZenore <163686699+DragonZenore@users.noreply.github.com>
2026-08-25 11:50:00 -0400

    fix trees (part 2)

--- mods/after_chill/scripts/world/tilesets/trees.lua
+++ mods/after_chill/scripts/world/tilesets/trees.lua
@@ -28,21 +28,21 @@ return {
     {
       id = 0,
       image = "../../../assets/sprites/tilesets/trees/tree.png",
-      width = 79,
-      height = 90,
+      width = 110,
+      height = 130,
       probability = -3
     },
     {
       id = 1,
       image = "../../../assets/sprites/tilesets/trees/rose.png",
       width = 41,
-      height = 68
+      height = 65
     },
     {
       id = 2,
       image = "../../../assets/sprites/tilesets/trees/tree2.png",
-      width = 83,
-      height = 79
+      width = 110,
+      height = 110
     }
   }
 }
