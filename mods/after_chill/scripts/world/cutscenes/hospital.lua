4efad03db207e7bba3a6cf73329cfadc56af1181
fluffyboy <sekalisukarumah@gmail.com>
2026-09-12 20:08:19 +1000

    Update hospital.lua

--- mods/after_chill/scripts/world/cutscenes/hospital.lua
+++ mods/after_chill/scripts/world/cutscenes/hospital.lua
@@ -3,8 +3,6 @@ return {
     patient0034 = function(cutscene) 
         local y = os.time() - 86400
         local formatted_date = os.date("%m/%d/%Y", y)
-        cutscene:text("* You read the sign.")
-        cutscene:wait(0.25)
         cutscene:text("* [voice:sign]\"Patient 0034 -[wait:2] Entered the hospital at " .. formatted_date .. ".\"")
         cutscene:text("* [voice:sign]\"Condition - Influenza.\"")
         cutscene:text("* [voice:sign]\"Don't give constant care,[wait:2] occasional check ups.\"[wait:3] \n* \"Provide mediocre food.\"")
